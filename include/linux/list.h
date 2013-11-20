c3addf7f5fea7028fc900b02ab7fab7acd4a7fc2
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-11-20 21:00:20 +0100

    Squashed update of kernel from 3.4.69 to 3.4.95
    Conflicts:
    	net/ipv6/route.c
    	fs/ext4/ialloc.c
    	security/selinux/hooks.c
    	net/core/filter.c
    	kernel/events/core.c
    
    This squashed update doesn't include the commits related to
    bluetooth drivers. The discarded commits are:
    ade988a Bluetooth: Fix missing length checks for L2CAP signalling PDUs
    6ec88fc Bluetooth: Fix invalid length check in l2cap_information_rsp()
    7f801fc Bluetooth: Support AR3011 in Acer Iconia Tab W500
    c2f15c3 Bluetooth: add support for atheros 0930:0219
    4126da5 Bluetooth: add support for atheros 0489:e057
    a1144ca Bluetooth: Add support for Foxconn/Hon Hai AR5BBU22 0489:E03C
    1403b36 Bluetooth: Add support for Sony Vaio T-Series
    ec5bf54 Bluetooth: Add USB_VENDOR_AND_INTERFACE_INFO() for Broadcom/Foxconn
    adebaa7 Bluetooth: Add support for BCM20702A0 [04ca, 2003]
    4a990f6 Bluetooth: Add support for BCM20702A0 [0b05, 17b5]
    3b36e95 Bluetooth: Add support for IMC Networks [13d3:3393]
    7a8c99e Bluetooth: Add support for GC-WB300D PCIe [04ca:3006] to ath3k.
    340733c Bluetooth: Add support for Foxconn / Hon Hai [0489:e04e]
    7a344a22 Bluetooth: Add support for Foxconn / Hon Hai [0489:e056]
    e98f05e Bluetooth device 04ca:3008 should use ath3k
    d83ff8c Bluetooth: Add support for atheros 04ca:3004 device to ath3k
    1a182cc Bluetooth: Device 0cf3:3008 should map AR 3012
    2737072 Bluetooth: Add support for Foxconn/Hon Hai [0489:e04d]
    5ff1715 Bluetooth: Add support for Mediatek Bluetooth device [0e8d:763f]
    61286bc Bluetooth: ath3k: Add support for Fujitsu Lifebook UH5x2 [04c5:1330]
    fa6e607 Bluetooth: ath3k: Add support for ID 0x13d3/0x3402
    d9b03b2 Bluetooth: Add support for Atheros [0cf3:3121]
    ae1c727 Bluetooth: Add support for Atheros [0cf3:e003]
    140deab Bluetooth: Add a new PID/VID 0cf3/e005 for AR3012.
    76b87de Bluetooth: Add support for BCM20702A0 [0b05, 17cb]

--- include/linux/list.h
+++ include/linux/list.h
@@ -371,24 +371,27 @@ static inline void list_splice_tail_init(struct list_head *list,
  */
 #define list_first_entry_or_null(ptr, type, member) \
 	(!list_empty(ptr) ? list_first_entry(ptr, type, member) : NULL)
+/**
+ * list_next_entry - get the next element in list
+ * @pos:	the type * to cursor
+ * @member:	the name of the list_struct within the struct.
+ */
+#define list_next_entry(pos, member) \
+	list_entry((pos)->member.next, typeof(*(pos)), member)
 
+/**
+ * list_prev_entry - get the prev element in list
+ * @pos:	the type * to cursor
+ * @member:	the name of the list_struct within the struct.
+ */
+#define list_prev_entry(pos, member) \
+	list_entry((pos)->member.prev, typeof(*(pos)), member)
 /**
  * list_for_each	-	iterate over a list
  * @pos:	the &struct list_head to use as a loop cursor.
  * @head:	the head for your list.
  */
 #define list_for_each(pos, head) \
-	for (pos = (head)->next; pos != (head); pos = pos->next)
-
-/**
- * __list_for_each	-	iterate over a list
- * @pos:	the &struct list_head to use as a loop cursor.
- * @head:	the head for your list.
- *
- * This variant doesn't differ from list_for_each() any more.
- * We don't do prefetching in either case.
- */
-#define __list_for_each(pos, head) \
 	for (pos = (head)->next; pos != (head); pos = pos->next)
 
 /**
